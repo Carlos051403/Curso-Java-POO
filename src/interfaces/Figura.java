0f9e28266ce100fe4433fc577e1ee52342bb633d
Carlos051403 <reyescarlos691@gmail.com>
2026-07-28 11:28:29 +0200

    feat(interfaces): crear interfaces Figura, Dibujable y Rotable con implementacion basica

---
+++ src/interfaces/Figura.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+public interface Figura {
+
+    //Usaremos los mismos ejercicios de clases abstractas
+    //Eliminamos todo y solo dejamos el metodo abstracto
+    //Cambiamos la palabra extends por interface
+
+    //aca no es necesario especificar la palabra abstract en los metodos
+    public double calcularArea();
+}
+
